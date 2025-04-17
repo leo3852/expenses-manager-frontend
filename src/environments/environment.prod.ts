a41a85bf15b2be5643e2b13c30d0beeca6dccbcb
leonardo gurgitano <leonardogurgi@gmail.com>
2025-04-18 00:10:50 +0100

    url environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://yourdomain.com/api' // Replace with your deployed backend URL
+    apiUrl: 'https://expenses-manager-production-1274.up.railway.app/api' // Replace with your deployed backend URL
 };
